7f897bf58660a5b0fc1a149142c71e9bc53ba527
S.A.S Sreyash Reddy <94442230+Sreyash-Reddy@users.noreply.github.com>
2025-06-23 23:54:10 +0530

    Revert "HOTFIX: Fix Failed to Generate Shipping ID Error"

--- src/main/java/com/adkhub/orders/service/OrderService.java
+++ src/main/java/com/adkhub/orders/service/OrderService.java
@@ -72,7 +72,7 @@ public class OrderService {
     }
 
     private String getApplicationId() {
-        return gcpSecretManagerService.getSecret(projectId, secretId, "1");
+        return gcpSecretManagerService.getSecret(projectId, secretId, "latest");
     }
 
     public String generateShippingID(UUID orderID) {
